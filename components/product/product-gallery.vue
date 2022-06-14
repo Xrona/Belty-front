5e59ad685d10bbb93dd865765dc09a36ff9c7cd5
erxweo <yizzr81@gmailc.on>
2022-06-14 13:53:06 +0400

    fix gallery

--- components/product/product-gallery.vue
+++ components/product/product-gallery.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
-    <vue-slick-carousel
-      :dots="true"
-      :arrows="false"
-      dots-class="gallery-dots"
-      :swipe-to-slide="true"
-    >
-      <template v-for="image of gallery">
-        <div :key="`gallery image ${image.id}`" class="gallery-image">
-          <img :src="image.url" alt="Product image" />
-        </div>
-      </template>
-      <template #customPaging>
-        <div class="rounded-full w-3 h-3 bg-gray-40"></div>
-      </template>
-    </vue-slick-carousel>
+    <template v-if="gallery.length > 0">
+      <vue-slick-carousel
+        :dots="true"
+        :arrows="false"
+        dots-class="gallery-dots"
+        :swipe-to-slide="true"
+      >
+        <template v-for="image of gallery">
+          <div :key="`gallery image ${image.id}`" class="gallery-image">
+            <img :src="image.url" alt="Product image" />
+          </div>
+        </template>
+        <template #customPaging>
+          <div class="rounded-full w-3 h-3 bg-gray-40"></div>
+        </template>
+      </vue-slick-carousel>
+    </template>
+    <template v-else>
+      <div>
+        <img src="null" class="gallery-image" alt="Product image" />
+      </div>
+    </template>
   </div>
 </template>
 
